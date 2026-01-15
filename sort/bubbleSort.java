ccd75227e2d3843f03296ad58c4960368594ee45
Gabriel Windlin <gawindlin@gmail.com>
2026-01-15 19:52:02 +0100

    bubble sort

--- sort/bubbleSort.java
+++ sort/bubbleSort.java
@@ -6,16 +6,20 @@ public class bubbleSort {
 
     // while 'j' > 'j+1' bubble it up
     for (int i = 0; i < arr.length; i++) {
+      boolean swapped = false;
       for (int j = 0; j < arr.length - 1 - i; j++) {
         if (arr[j] > arr[j + 1]) {
           // swap
           int temp = arr[j + 1];
           arr[j + 1] = arr[j];
           arr[j] = temp;
+          swapped = true;
         }
       }
+      // if we didnt swap anything -> it is sorted
+      if (!swapped)
+        break;
     }
-
   }
 
   public static void main(String[] args) {
