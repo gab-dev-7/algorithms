7d50d147728ec23e5974d27c1b99fdb872d41030
Gabriel Windlin <gawindlin@gmail.com>
2026-01-15 19:45:45 +0100

    bubble sort

---
+++ sort/bubbleSort.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+public class bubbleSort {
+
+  public static void sort(int[] arr) {
+
+    // while 'j' > 'j+1' bubble it up
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = 0; j < arr.length - 1; j++) {
+        if (arr[j] > arr[j + 1]) {
+          // swap
+          int temp = arr[j + 1];
+          arr[j + 1] = arr[j];
+          arr[j] = temp;
+        }
+      }
+    }
+
+  }
+
+  public static void main(String[] args) {
+    int[] data = { 64, 34, 25, 12, 22, 11, 90 };
+    System.out.println("Original Array: " + Arrays.toString(data));
+
+    sort(data);
+
+    System.out.println("Sorted Array:   " + Arrays.toString(data));
+
+    // Verification
+    int[] expected = { 11, 12, 22, 25, 34, 64, 90 };
+    System.out.println("Sort Successful: " + Arrays.equals(data, expected));
+  }
+}
