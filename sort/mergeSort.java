1c922bc45bb886e13d61edbff27285fbc21c8b10
Gabriel Windlin <gawindlin@gmail.com>
2026-01-19 20:48:18 +0100

    prevent overflow

--- sort/mergeSort.java
+++ sort/mergeSort.java
@@ -14,7 +14,7 @@ public class mergeSort {
 
   private static void mergeSort(int[] arr, int[] temp, int left, int right) {
     if (left < right) {
-      int mid = (left + right) / 2;
+      int mid = left + (right - left) / 2;
 
       // Sort the left half recursively
       mergeSort(arr, temp, left, mid);
